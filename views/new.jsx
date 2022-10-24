735d2076d2fa90f0d0355567441cde23aa9b7e1c
Davin <davin-dj@hotmail.com>
2022-10-24 18:42:58 -0400

    Delete route completed

--- views/new.jsx
+++ views/new.jsx
@@ -18,6 +18,7 @@ function New() {
                     type="text"
                     name="image"
                     id="image"
+                    pattern="https?://.+" title="Include http://"
                 />
                 <label htmlFor="hasGluten">Has Gluten?</label>
                 <input
