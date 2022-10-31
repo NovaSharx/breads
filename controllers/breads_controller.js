8c3e7fe445b35439f902d9c36984bf9f34f509e8
Davin <davin-dj@hotmail.com>
2022-10-31 19:04:06 -0400

    Refactored delete, edit and update routes to use mongoose

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const SampleBreads = require('../models/samplebreads.js')
 
 // INDEX
 breads.get('/', (req, res) => {
@@ -20,42 +21,57 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// MULTIPLE NEW
+breads.get('/data/seed', (req, res) => {
+    Bread.insertMany(SampleBreads)
+        .then(createdBreads => {
+            res.redirect('/breads')
+        })
+})
+
 // SHOW
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
-    .then(foundBread => {
-        res.render('show', {
-            bread: foundBread
+        .then(foundBread => {
+            res.render('show', {
+                bread: foundBread
+            })
         })
-    })
-    .catch(err => {
-        res.render('error404')
-    })
+        .catch(err => {
+            res.render('error404')
+        })
 })
 
 //EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-    res.render('edit', {
-        bread: Bread[req.params.indexArray],
-        index: req.params.indexArray
-    })
+breads.get('/:id/edit', (req, res) => {
+    Bread.findById(req.params.id)
+        .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread
+            })
+        })
 })
 
 //UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
     if (req.body.hasGluten === 'on') {
         req.body.hasGluten = true
     } else {
         req.body.hasGluten = false
     }
-    Bread[req.params.arrayIndex] = req.body
-    res.redirect(`/breads/${req.params.arrayIndex}`)
+    Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(updatedBread => {
+            console.log(updatedBread)
+            res.redirect(`/breads/${req.params.id}`)
+        })
 })
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-    Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+    Bread.findByIdAndDelete(req.params.id)
+        .then(deletedBread => {
+            res.status(303).redirect('/breads')
+        })
 })
 
 // CREATE
--- views/show.jsx
+++ views/show.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 const breads = require('../controllers/breads_controller')
 const Default = require('./layouts/Default')
 
-function Show({bread, index}) {
+function Show({bread}) {
     return (
         <Default>
             <h2>Show Page</h2>
@@ -18,9 +18,9 @@ function Show({bread, index}) {
             </p>
             <img src={bread.image} alt={bread.name} />
 
-            <a href={`/breads/${index}/edit`}> <button>Edit</button> </a>
+            <a href={`/breads/${bread.id}/edit`}> <button>Edit</button> </a>
 
-            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+            <form action={`/breads/${bread.id}?_method=DELETE`} method='POST'>
                 <input type="submit" value="DELETE" />
             </form>
         </Default>
